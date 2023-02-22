1ebbbadb1e44f15a6b2c9046e578cf86debd4eb8
Dan Chilibeck <dan.chilibeck@gmail.com>
2023-02-22 15:43:19 -0400

    added unit testing capability to solution

---
+++ CustomWorkflowUnitTestProject/CustomWorkflowUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FakeItEasy;
+using FakeXrmEasy;
+
+using System.Collections.Generic;
+using CustomWorkflow;
+
+
+namespace CustomWorkflowUnitTestProject
+{
+    [TestClass]
+    public class CustomWorkflowUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var fakedContext = new XrmFakedContext();
+
+            //Inputs
+            var inputs = new Dictionary<string, object> {
+            {"InputText", "The quick fox jumped over the lazy brown prime minister"},
+        };
+
+            var result = fakedContext.ExecuteCodeActivity<WordCount>(inputs);
+
+            Assert.IsTrue(result["CountofWords"].Equals(10));
+           
+        }
+    }
+}
